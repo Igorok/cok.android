22973ad9e7509e296a073deec6ab339272bb3d2d
igor <igordubinin1@yandex.ru>
2015-01-05 23:27:31 +0300

    fixes

--- app/src/main/java/ru/igor_ok/cokandroid/LoginActivity.java
+++ app/src/main/java/ru/igor_ok/cokandroid/LoginActivity.java
@@ -2,8 +2,6 @@ package ru.igor_ok.cokandroid;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
@@ -18,26 +16,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 
 public class LoginActivity extends ActionBarActivity {
@@ -102,30 +92,6 @@ public class LoginActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
-    /*public void postData() {
-        // Create a new HttpClient and Post Header
-        HttpClient httpclient = new DefaultHttpClient();
-        HttpPost httppost = new HttpPost("http://localhost:3000");
-
-        try {
-            // Add your data
-            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-            nameValuePairs.add(new BasicNameValuePair("id", "12345"));
-            nameValuePairs.add(new BasicNameValuePair("stringdata", "AndDev is Cool!"));
-            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-
-            // Execute HTTP Post Request
-            HttpResponse response = httpclient.execute(httppost);
-
-        } catch (ClientProtocolException e) {
-            // TODO Auto-generated catch block
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-        }
-    }*/
-
-
     public static String POST(String url, String login, String password){
         InputStream inputStream = null;
         String result = "";
@@ -175,9 +141,8 @@ public class LoginActivity extends ActionBarActivity {
 //        SharedPreferences.Editor editor = settings.edit();
         @Override
         protected String doInBackground(String... urls) {
-            return POST("http://192.168.0.64:3000/jsonrpc", getLogin, getPassword);
+            return POST(urls[0], getLogin, getPassword);
         }
-        // onPostExecute displays the results of the AsyncTask.
         @Override
         protected void onPostExecute(String result) {
             loginMessage.setText(result);
@@ -198,185 +163,3 @@ public class LoginActivity extends ActionBarActivity {
         }
     }
 }
-
-
-
-
-/*
-
-package com.hmkcode.android;
-         
-        import java.io.BufferedReader;
-        import java.io.IOException;
-        import java.io.InputStream;
-        import java.io.InputStreamReader;
-        import org.apache.http.HttpResponse;
-        import org.apache.http.client.HttpClient;
-        import org.apache.http.client.methods.HttpPost;
-        import org.apache.http.entity.StringEntity;
-        import org.apache.http.impl.client.DefaultHttpClient;
-        import org.json.JSONObject;
-        import android.net.ConnectivityManager;
-        import android.net.NetworkInfo;
-        import android.os.AsyncTask;
-        import android.os.Bundle;
-        import android.util.Log;
-        import android.view.View;
-        import android.view.View.OnClickListener;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.TextView;
-        import android.widget.Toast;
-        import android.app.Activity;
-        import com.hmkcode.android.vo.Person;
-         
-public class MainActivity extends Activity implements OnClickListener {
-     
-                TextView tvIsConnected;
-        EditText etName,etCountry,etTwitter;
-        Button btnPost;
-     
-                Person person;
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_main);
-         
-                // get reference to the views
-                tvIsConnected = (TextView) findViewById(R.id.tvIsConnected);
-                etName = (EditText) findViewById(R.id.etName);
-                etCountry = (EditText) findViewById(R.id.etCountry);
-                etTwitter = (EditText) findViewById(R.id.etTwitter);
-                btnPost = (Button) findViewById(R.id.btnPost);
-         
-                // check if you are connected or not
-                if(isConnected()){
-                        tvIsConnected.setBackgroundColor(0xFF00CC00);
-                        tvIsConnected.setText("You are conncted");
-                    }
-                else{
-                        tvIsConnected.setText("You are NOT conncted");
-                    }
-         
-                // add click listener to Button "POST"
-                btnPost.setOnClickListener(this);
-         
-            }
-     
-                public static String POST(String url, Person person){
-                InputStream inputStream = null;
-                String result = "";
-                try {
-             
-                        // 1. create HttpClient
-                        HttpClient httpclient = new DefaultHttpClient();
-             
-                        // 2. make POST request to the given URL
-                        HttpPost httpPost = new HttpPost(url);
-             
-                        String json = "";
-             
-                        // 3. build jsonObject
-                        JSONObject jsonObject = new JSONObject();
-                        jsonObject.accumulate("name", person.getName());
-                        jsonObject.accumulate("country", person.getCountry());
-                        jsonObject.accumulate("twitter", person.getTwitter());
-             
-                        // 4. convert JSONObject to JSON to String
-                        json = jsonObject.toString();
-             
-                        // ** Alternative way to convert Person object to JSON string usin Jackson Lib
-                        // ObjectMapper mapper = new ObjectMapper();
-                        // json = mapper.writeValueAsString(person);
-             
-                        // 5. set json to StringEntity
-                        StringEntity se = new StringEntity(json);
-             
-                        // 6. set httpPost Entity
-                        httpPost.setEntity(se);
-             
-                        // 7. Set some headers to inform server about the type of the content   
-                        httpPost.setHeader("Accept", "application/json");
-                        httpPost.setHeader("Content-type", "application/json");
-             
-                        // 8. Execute POST request to the given URL
-                        HttpResponse httpResponse = httpclient.execute(httpPost);
-             
-                        // 9. receive response as inputStream
-                        inputStream = httpResponse.getEntity().getContent();
-             
-                        // 10. convert inputstream to string
-                        if(inputStream != null)
-                            result = convertInputStreamToString(inputStream);
-                        else
-                            result = "Did not work!";
-             
-                    } catch (Exception e) {
-                        Log.d("InputStream", e.getLocalizedMessage());
-                    }
-         
-                // 11. return result
-                return result;
-            }
-     
-                public boolean isConnected(){
-                ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
-                    NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-                    if (networkInfo != null && networkInfo.isConnected())
-                        return true;
-                    else
-                        return false;    
-            }
-        @Override
-        public void onClick(View view) {
-         
-                switch(view.getId()){
-                        case R.id.btnPost:
-                                if(!validate())
-                                    Toast.makeText(getBaseContext(), "Enter some data!", Toast.LENGTH_LONG).show();
-                                // call AsynTask to perform network operation on separate thread
-                                new HttpAsyncTask().execute("http://hmkcode.appspot.com/jsonservlet");
-                            break;
-                    }
-         
-            }
-        private class HttpAsyncTask extends AsyncTask<String, Void, String> {
-                @Override
-                protected String doInBackground(String... urls) {
-             
-                        person = new Person();
-                        person.setName(etName.getText().toString());
-                        person.setCountry(etCountry.getText().toString());
-                        person.setTwitter(etTwitter.getText().toString());
-             
-                        return POST(urls[0],person);
-                    }
-                // onPostExecute displays the results of the AsyncTask.
-                        @Override
-                protected void onPostExecute(String result) {
-                        Toast.makeText(getBaseContext(), "Data Sent!", Toast.LENGTH_LONG).show();
-                   }
-            }
-     
-                private boolean validate(){
-                if(etName.getText().toString().trim().equals(""))
-                    return false;
-                else if(etCountry.getText().toString().trim().equals(""))
-                    return false;
-                else if(etTwitter.getText().toString().trim().equals(""))
-                    return false;
-                else
-                    return true;    
-            }
-        private static String convertInputStreamToString(InputStream inputStream) throws IOException{
-                BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
-                String line = "";
-                String result = "";
-                while((line = bufferedReader.readLine()) != null)
-                    result += line;
-         
-                inputStream.close();
-                return result;
-         
-            }   
-}*/
